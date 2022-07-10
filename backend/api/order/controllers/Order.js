63caf923e0a5b9f9a732bf18724cdcad400217e3
Rachel Weisberger <rachel.weisberger@gmail.com>
2022-07-10 15:40:17 -0600

    first commit

---
+++ backend/api/order/controllers/Order.js
@@ -0,0 +1,45 @@
+"use strict";
+/**
+ * Order.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Order`.
+ */
+// note that this needs to be a "private" key from STRIPE
+const stripe = require("stripe")(
+  "sk_test_51LJjbUHVRxsao4VXRaKfIQ4XyMRv8x6jxr4bghSz4mCeGq95YuYR90nq1FqBs5ghbW0RLB3oO7Ht2fo1IkQkBZE900alD2uFDu"
+);
+module.exports = {
+  /**
+   * Create a/an order record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    const { address, amount, dishes, token, city, state } = JSON.parse(
+      ctx.request.body
+    );
+    const stripeAmount = Math.floor(amount * 100);
+    // charge on stripe
+    const charge = await stripe.charges.create({
+      // Transform cents to dollars.
+      amount: stripeAmount,
+      currency: "usd",
+      description: `Order ${new Date()} by ${ctx.state.user._id}`,
+      source: token,
+    });
+
+    // Register the order in the database
+    const order = await strapi.services.order.create({
+      user: ctx.state.user.id,
+      charge_id: charge.id,
+      amount: stripeAmount,
+      address,
+      dishes,
+      city,
+      state,
+    });
+
+    return order;
+  },
+};
